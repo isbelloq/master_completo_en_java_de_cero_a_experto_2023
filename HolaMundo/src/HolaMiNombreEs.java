bbb6f404a52c31749e21349394ebec48c256a48d
Santiago Bello <santiago.bello@mercadolibre.com.co>
2023-08-17 22:32:45 -0500

    Seccion 1 y 2 Hola mundo y ejemplos de variables

---
+++ HolaMundo/src/HolaMiNombreEs.java
@@ -0,0 +1,6 @@
+public class HolaMiNombreEs {
+    public static void main(String[] args){
+        String nombre = "Santiago";
+        System.out.println("Hola, mi nombre es " + nombre);
+    }
+}
---
+++ HolaMundo/src/HolaMundo.java
@@ -0,0 +1,19 @@
+public class HolaMundo {
+    public static void main(String[] args){
+        String saludar = "Hola Mundo desde Java";
+        System.out.println("saludar.toUpperCase() = " + saludar.toUpperCase());
+
+        int numero = 10;
+        System.out.println("numero = " + numero);
+
+        boolean valor = true;
+        int numero2 = 5;
+        if(valor){
+            System.out.println("numero = " + numero);
+            numero2 = 10;
+        }
+        System.out.println("numero2 = " + numero2);
+
+        var numero3 = "15";
+    }
+}
