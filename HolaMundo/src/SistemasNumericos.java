d41d3997a693ea3bc6ac9b5747adcc80e1fd5a63
Santiago Bello <santiago.bello@mercadolibre.com.co>
2023-08-20 11:44:24 -0500

    Seccion 2 entradas de datos desde ventana de dialogo

--- HolaMundo/src/SistemasNumericos.java
+++ HolaMundo/src/SistemasNumericos.java
@@ -1,18 +1,35 @@
+import javax.swing.*;
+
 public class SistemasNumericos{
     public static void main(String[] args) {
-        int numeroDecimal = 500;
+        String numeroStr = JOptionPane.showInputDialog(null, "Ingrese un numero entero");
+
+        int numeroDecimal = 0;
+
+        try{
+            numeroDecimal = Integer.parseInt(numeroStr);
+        }
+        catch(NumberFormatException e){
+            JOptionPane.showMessageDialog(null, "Error, debe ingresar un numero entero");
+            main(args);
+            System.exit(0);
+        };
+
         System.out.println("numeroDecinal = " + numeroDecimal);
 
-        System.out.println("numero binario de " + numeroDecimal + " = " + Integer.toBinaryString(numeroDecimal));
-        int numeroBinario = 0b111110100;
-        System.out.println("numeroBinario = " + numeroBinario);
+        String resultadoBinario = "numero binario de " + numeroDecimal + " = " + Integer.toBinaryString(numeroDecimal);
+        System.out.println(resultadoBinario);
 
-        System.out.println("numero octal de " + numeroDecimal + " = " + Integer.toOctalString(numeroDecimal));
-        int numeroOctal = 0764;
-        System.out.println("numeroOctal = " + numeroOctal);
+        String resultadoOctal = "numero octal de " + numeroDecimal + " = " + Integer.toOctalString(numeroDecimal);
+        System.out.println(resultadoOctal);
 
-        System.out.println("numero hexadecimal de " + numeroDecimal + " = " + Integer.toHexString(numeroDecimal));
-        int numeroHex = 0x1f4;
-        System.out.println("numeroHex = " + numeroHex);
+        String resultadoHexadecimal = "numero hexadecimal de " + numeroDecimal + " = " + Integer.toHexString(numeroDecimal);
+        System.out.println(resultadoHexadecimal);
+
+        String mensaje = resultadoBinario;
+        mensaje += "\n" + resultadoOctal;
+        mensaje += "\n" + resultadoHexadecimal;
+
+        JOptionPane.showMessageDialog(null, mensaje);
     }
 }
