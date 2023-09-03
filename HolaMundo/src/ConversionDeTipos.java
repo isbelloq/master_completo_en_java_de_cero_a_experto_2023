cd9beb267db800afd2b76a82e008ced15a4aea5f
Santiago Bello <santiago.bello@mercadolibre.com.co>
2023-09-03 17:41:03 -0500

    seccion 2 tarea 2 detalle de la factura

--- HolaMundo/src/ConversionDeTipos.java
+++ HolaMundo/src/ConversionDeTipos.java
@@ -12,5 +12,31 @@ public class ConversionDeTipos{
         String logicoStr = "TruE";
         boolean logicoBoolean = Boolean.parseBoolean(logicoStr);
         System.out.println("logicoBoolean = " + logicoBoolean);
+
+        int otroNumeroInt = 100;
+        System.out.println("otroNumeroInt = " + otroNumeroInt);
+        String otroNumeroStr = Integer.toString(otroNumeroInt);
+        System.out.println("otroNumeroStr = " + otroNumeroStr);
+
+        otroNumeroStr = String.valueOf(otroNumeroInt + 10);
+        System.out.println("otroNumeroStr = " + otroNumeroStr);
+
+        double otroRealDouble = 1.23456e2;
+        String otroRealStr = Double.toString(otroRealDouble);
+        System.out.println("otroRealStr = " + otroRealStr);
+
+        otroRealStr = String.valueOf(1.23456e2f);
+        System.out.println("otroRealStr = " + otroRealStr);
+
+        int i = 32768;
+        short s = (short)i;
+        System.out.println("s = " + s);
+        long l = i;
+        System.out.println("l = " + l);
+        System.out.println(Short.MAX_VALUE);
+        char c = (char) i;
+        System.out.println("c = " + c);
+        float f = (float) i;
+        System.out.println("f = " + f);
     }
 }
