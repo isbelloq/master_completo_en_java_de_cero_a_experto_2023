4cd7addc7370ee3147338335b2fce9b927d4f803
Santiago Bello <santiago.bello@mercadolibre.com.co>
2024-03-11 08:42:46 -0500

    :sparkles: Swich case ejemplo número de dias del mes

---
+++ HolaMundo/src/SentenciaSwitchCaseNumDiasMes.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class SentenciaSwitchCaseNumDiasMes {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Ingrese el mes (1 - 12)");
+        int mes = s.nextInt();
+
+        int numeroDias = 0;
+        System.out.println("Ingrese el año (YYYY)");
+        int anho = s.nextInt();
+
+        switch (mes){
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                numeroDias = 31;
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                numeroDias = 30;
+                break;
+            case 2:
+                if (anho % 400 == 0 || ((anho % 4 == 0) && !(anho % 100 == 0))){
+                    numeroDias = 29;
+                }
+                else{
+                    numeroDias = 28;
+                }
+                break;
+            default:
+                numeroDias = 0;
+        }
+
+        System.out.println("numeroDias = " + numeroDias);
+    }
+}
