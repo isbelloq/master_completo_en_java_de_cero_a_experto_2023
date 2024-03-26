96919d3b053ab60eb3a7ed3e6c06c5263c1ec24c
Santiago Bello <santiago.bello@mercadolibre.com.co>
2024-03-26 16:55:49 -0500

    :sparkles: Autoboxing y unboxing

---
+++ HolaMundo/src/AutoboxingInteger.java
@@ -0,0 +1,23 @@
+public class AutoboxingInteger {
+    public static void main(String[] args) {
+        Integer[] enteros = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+
+        int sum = 0;
+
+        for (Integer i: enteros){
+            if (i.intValue() % 2 == 0){
+                sum += i.intValue();
+            }
+        }
+
+        System.out.println("suma = " + sum);
+
+        sum = 0;
+        for (Integer i: enteros){
+            if (i % 2 == 0){
+                sum += i;
+            }
+        }
+        System.out.println("suma = " + sum);
+    }
+}
